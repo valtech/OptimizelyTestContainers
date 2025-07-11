38da0b6183f558885ce424e26487dc0ce3e9e663
Andreas Ylivainio <andreas.ylivainio@valtech.com>
2025-07-11 13:51:47 +0200

    Use one container with named databases and fix connection string issues

--- test/OptimizelyTestContainers.Tests/OptimizelyIntegrationTestBase.cs
+++ test/OptimizelyTestContainers.Tests/OptimizelyIntegrationTestBase.cs
@@ -1,7 +1,8 @@
-﻿using EPiServer.Data;
-using EPiServer.Framework;
+﻿using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
+using Mediachase.Data.Provider;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,61 +15,53 @@ public class OptimizelyIntegrationTestBase(bool includeCommerce) : IAsyncLifetim
 {
     private IHost _host = null!;
 
-    private MsSqlContainer _cmsDbContainer = null!;
+    // Since we use same container with different db names we can remove one of these :P
+    private MsSqlContainer _databaseContainer = null!;
 
-    private MsSqlContainer _commerceDbContainer = null!;
-    
     protected IServiceProvider Services { get; private set; } = null!;
-
+    
     public virtual async Task InitializeAsync()
     {
         // Create Cms SQL Server container
-        _cmsDbContainer = CreateNamedSqlContainer( "Cms");
+        var container = await CreateDatabaseContainer();
         
-        // Create Commerce SQL Server container
+        // Create CMS databse
+        var cmsDatabaseConnectionString = await CreateNamedDatabaseConnectionString(container, "Cms");
+
+        string? commerceDatabaseConnectionString = null;
         if (includeCommerce)
         {
-            _commerceDbContainer = CreateNamedSqlContainer("Commerce");
+            commerceDatabaseConnectionString = await CreateNamedDatabaseConnectionString(container, "Commerce");
         }
         
-        // Start database containers
-        await _cmsDbContainer.StartAsync();
-
-        if (includeCommerce)
-        {
-            await _commerceDbContainer.StartAsync();
-        }
-
+        
         // Build CMS host
         _host = Host.CreateDefaultBuilder()
-            .ConfigureCmsDefaults()
-            .ConfigureWebHostDefaults(webHostBuilder =>
+           .ConfigureWebHostDefaults(webHostBuilder =>
             {
-                webHostBuilder.ConfigureServices((context, services) =>
-                {
-                    services.Configure<DataAccessOptions>(opt =>
+                webHostBuilder
+                    .ConfigureServices((context, services) =>
                     {
-                        var containerConnectionString = _cmsDbContainer.GetConnectionString();
+                        // Add data importer service to setup default content for the tests
+                        services.AddTransient<OptimizelyDataImporter>();
+                    })
+                    .ConfigureAppConfiguration((context, configBuilder) =>
+                    {
+                        // Workaround to set separate database names inisde container
+                        if (includeCommerce && !string.IsNullOrWhiteSpace(commerceDatabaseConnectionString))
+                        {
+                            var testSettings = new Dictionary<string, string?>
+                            {
+                                // TODO: Find Constant for connection string!
+                                ["ConnectionStrings:EPiServerDB"] = cmsDatabaseConnectionString,
+                                ["ConnectionStrings:EcfSqlConnection"] = commerceDatabaseConnectionString,
+                            };
 
-                        opt.SetConnectionString(containerConnectionString);
+                            configBuilder.AddInMemoryCollection(testSettings);
+                        }
                     });
-            
-                    // Add data importer service to setup default content for the tests
-                    services.AddTransient<OptimizelyDataImporter>();
-                });
-
-                webHostBuilder.ConfigureAppConfiguration((context, configBuilder) =>
-                {
-                    var testSettings = new Dictionary<string, string?>
-                    {
-                        // TODO: Find Constant for connection string!
-                        ["ConnectionStrings:EcfSqlConnection"] = _commerceDbContainer.GetConnectionString()
-                    };
-
-                    configBuilder.AddInMemoryCollection(testSettings);
-                });
-
-                if (includeCommerce)
+                
+                if (includeCommerce && !string.IsNullOrWhiteSpace(commerceDatabaseConnectionString))
                 {
                     webHostBuilder.UseStartup<StartupWithCmsAndCommerce>();
                 }
@@ -78,9 +71,13 @@ public class OptimizelyIntegrationTestBase(bool includeCommerce) : IAsyncLifetim
                 }
 
             })
-            .Build();
+            .ConfigureCmsDefaults()
+           .Build();
         
-        // Run initialization engine (simulate application startup)
+        // Run initialization engine (simulate application startup) 
+        
+        // TODO: Runs all initializable modules even if commerce is not included!
+        // Solve with custom IAssemblyScanner?
         var initializer = _host.Services.GetRequiredService<InitializationEngine>();
         if (initializer.InitializationState != InitializationState.Initialized)
             initializer.Initialize(); 
@@ -94,20 +91,34 @@ public class OptimizelyIntegrationTestBase(bool includeCommerce) : IAsyncLifetim
     {
         await _host.StopAsync();
         
-        await _cmsDbContainer.DisposeAsync();
-
-        if (includeCommerce)
-        {
-            await _commerceDbContainer.DisposeAsync();
-        }
+        await _databaseContainer.DisposeAsync();
     }
     
-    private MsSqlContainer CreateNamedSqlContainer(string componentName)
+    private async Task<MsSqlContainer> CreateDatabaseContainer()
     {
-        return new MsSqlBuilder()
-            .WithName(componentName + GetType()) // Unique name per test class and component
+        var container = new MsSqlBuilder()
             .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
             .WithPassword("yourStrong(!)Password")
             .Build();
+
+        await container.StartAsync();
+        
+        _databaseContainer = container;
+        
+        return _databaseContainer;
+    }
+
+    private async Task<string> CreateNamedDatabaseConnectionString(MsSqlContainer container, string databaseName)
+    {
+        databaseName = $"{GetType().Name}-{databaseName}";
+        
+        var masterConnectionString = container.GetConnectionString();
+        await using var connection = new SqlConnection(masterConnectionString);
+        await connection.OpenAsync();
+        await using var command = new SqlCommand($"CREATE DATABASE [{databaseName}]", connection);
+        await command.ExecuteNonQueryAsync();
+
+        // Workaround to set separate database names inside container
+        return masterConnectionString.Replace("master", databaseName);
     }
 }
